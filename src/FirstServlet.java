8515098e001003e3921d5e0f574fd9893b2ba4ca
Nesterovich Alexsander <nesterovich.aleksandr.a@gmail.com>
2023-12-07 15:46:48 +0300

    add first servlet

---
+++ src/FirstServlet.java
@@ -0,0 +1,16 @@
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println(Runner.getCustomer());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    }
+}
--- src/Runner.java
+++ src/Runner.java
@@ -10,15 +10,16 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
+        System.out.println(getCustomer());
+    }
+
+    public static Customer getCustomer() {
         Honey honey = new Honey(LocalDate.of(2023, 6, 8));
         List<Product> products = new ArrayList<>();
         products.add(honey);
         Order order = new Order(1L, LocalDate.now(), products, BigDecimal.valueOf(20));
         List<Order> orders = new ArrayList<>();
         orders.add(order);
-        Customer customer = new Customer("Alex", 375291572087L, orders);
-        System.out.println(customer);
-
-
+        return new Customer("Alex", 375291572087L, orders);
     }
 }
